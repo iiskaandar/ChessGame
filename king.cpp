f55e3c180c349d5cc4fda1b8a30c4efebaac9560
aleksandra.kasprzak <aleksandra.kasprzak@espeo.eu>
2020-04-13 21:13:49 +0200

    king moves

--- king.cpp
+++ king.cpp
@@ -5,4 +5,74 @@ using namespace std;
 string King::getName(){
     return ("Król");
 }
+array<array<bool,8>,8> King::checkPossibleMove(array<int, 2> position,  array<array<Pawn*,8>,8> ChessBoard){
+    array<array<bool,8>,8> possibleMoves;
+    string color = ChessBoard[position[0]][position[1]]->getColor();
+    string actualColor;
+
+    for(int row=0; row<8; row++){
+        for(int col=0; col<8; col++){
+            possibleMoves[row][col] = false;
+        }
+    }
+    //góra
+    if((position[0] + 1) < 8){
+      actualColor = ChessBoard[position[0] + 1][position[1]]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0] + 1][position[1]] = true;
+      }
+    }
+    //dół
+    if((position[0] - 1) >= 0){
+      actualColor = ChessBoard[position[0] - 1][position[1]]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0] - 1][position[1]] = true;
+      }
+    }
+    //lewo
+    if((position[1] - 1) >= 0){
+      actualColor = ChessBoard[position[0]][position[1]-1]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0]][position[1]-1] = true;
+      }
+    }
+    //prawo
+    if((position[1] + 1) < 8){
+      actualColor = ChessBoard[position[0]][position[1]+1]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0]][position[1]+1] = true;
+      }
+    }
+    //prawa góra
+    if((position[0] + 1) < 8 && (position[1] + 1) < 8){
+      actualColor = ChessBoard[position[0]+1][position[1]+1]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0]+1][position[1]+1] = true;
+      }
+    }
+    //prawy dół
+    if((position[0] - 1) >= 0 && (position[1] + 1) < 8){
+      actualColor = ChessBoard[position[0]-1][position[1]+1]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0]-1][position[1]+1] = true;
+      }
+    }
+    //lewa góra
+    if((position[0] + 1) < 8 && (position[1] - 1) >= 0){
+      actualColor = ChessBoard[position[0]+1][position[1]-1]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0]+1][position[1]-1] = true;
+      }
+    }
+    //lewy dół
+    if((position[0] - 1) >= 0 && (position[1] - 1) >= 0){
+      actualColor = ChessBoard[position[0]-1][position[1]-1]->getColor();
+      if(actualColor != color){
+          possibleMoves[position[0]-1][position[1]-1] = true;
+      }
+    }
+
+
+    return possibleMoves;
+}
 
--- king.h
+++ king.h
@@ -6,6 +6,7 @@ class King : public Pawn
 {
 public:
     string getName() override;
+    array<array<bool,8>,8> checkPossibleMove(array<int, 2> position, array<array<Pawn*,8>,8> ChessBoard) override;
     int whiteStartRow = 7;
     int whiteStartColumn = 4;
     int blackStartRow = 0;
